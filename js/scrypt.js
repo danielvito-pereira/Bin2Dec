7666a3d0fb159fc0a8d3705d41cb2289096fef18
danielvito-pereira <danielvitor081@gmail.com>
2022-12-12 23:44:37 -0300

    re

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,35 +1,29 @@
-  let binary = document.getElementById('binary').value;
+  let binary = document.getElementById('binary');
   const btn = document.getElementById('btn')
-
-
-
-function binaryToDecimal(binary) {
-  let t = binary;
-  let decimal_v=0;
-  let = base = 1;
-  while (t) {
-    let ult_num = t % 10;
-    t = Math.floor(temp / 10);
+  const output = document.querySelector("#output");
+  const error = document.querySelector("#error-msg");
   
-    decimal_v+= ult_num * base;
-    base = base *2 ;
-  
-  } 
-  document.getElementById("output").innerHTML = decimal_v;
-  console.log(decimal_v);
+
+function convertBinarioForDecimal() {
+  const regEx = /^[0-1]+$/;
+
+  if(binary.value.match(regEx)) {
+    const binarys = binary.value.split('').reverse();
+    let decNo = 0;
+    binarys.forEach((item, index) => item === '1' ? decNo += Math.pow(2, index) : void 0);
+
+    output.value = decNo.toString();
+    output.style.cursor = 'text';
+  } else {
+    error.style.display = 'block';
+}
+
+btn.addEventListener('click', () => {
+  error.style.display = 'none';
+
+  convertBinarioForDecimal();
+  console.log(  convertBinarioForDecimal())
+});
 }
 
 
-btn.addEventListener('click', () => {
-
-  console.log(binaryToDecimal())
-  
-});
-
-
-/*A ideia é extrair os dígitos de um determinado número binário a partir do dígito mais à direita e manter uma variável dec_value. No momento de extrair dígitos do número binário, multiplique o dígito com a base adequada (Potência de 2) e adicione-o à variável dec_value. No final, a variável dec_value armazenará o número decimal necessário.
-Por exemplo: 
-se o número binário for 111. 
-dec_value = 1 * (2 ^ 2) + 1 * (2 ^ 1) + 1 * (2 ^ 0) = 7
-
-O diagrama abaixo explica como converter (1010) em valor decimal equivalente: */
